0d8d1e0ee12da3daaf196201a6679ef0ed7d9ca1
smignot <soline.mignot@ensae.fr>
2023-11-21 15:10:27 +0000

    Changements des noms des fichiers après 2016

--- python.py
+++ python.py
@@ -6,16 +6,23 @@ path="/home/onyxia/work/projet-python/"
 
 
 def dataset_per_year(year):
-    file_name1="carcteristiques-"+year+".csv"
+    file_name1="carcteristiques_"+year+".csv"
     df= pd.read_csv(path+file_name1, sep=';')
-    file_name2="lieux-"+year+".csv"
+    file_name2="lieux_"+year+".csv"
     df2= pd.read_csv(path+file_name2, sep=';',low_memory=False) 
     df=pd.concat([df,df2],axis=1)
-    file_name3="usagers-"+year+".csv"
+    file_name3="usagers_"+year+".csv"
 
-    file_name4="vehicules-"+year+".csv"
+    file_name4="vehicules_"+year+".csv"
 
     return df
+
+def complete_dataset():
+    df=dataset_per_year("2005")
+    for i in range (2006,2023):
+        df=pd.concat([df,dataset_per_year(str(i))])
+    return df
+
 
 df_2022=dataset_per_year("2022")
 
