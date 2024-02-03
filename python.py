e59e2f706b3e9693b73e0a9c177c4e4e3f407c2b
smignot <soline.mignot@ensae.fr>
2024-02-03 15:01:42 +0000

    je tente de fixer les gravités

--- python.py
+++ python.py
@@ -4,41 +4,34 @@ import matplotlib.pyplot as plt
 
 path="/home/onyxia/projet-python/"
 
-def caracteristiques():
-    file_name1="caracteristiques_"+"2005"+".csv"
-    df= pd.read_csv(path+file_name1, sep=',',low_memory=False)
-    for i in range (2006,2023):
-        file_name1="caracteristiques_"+str(year)+".csv"
-        df2= pd.read_csv(path+file_name1, sep=',',low_memory=False)
-        df=pd.concat([df,df2])
-        print(i)
-    return df
 
 def dataset_per_year(year):
-    file_name1="caracteristiques_"+year+".csv"
+    file_name1="caracteristiques_"+str(year)+".csv"
     df= pd.read_csv(path+file_name1, sep=',',low_memory=False)
-    file_name2="lieux_"+year+".csv"
+    file_name2="lieux_"+str(year)+".csv"
     df2= pd.read_csv(path+file_name2, sep=',',low_memory=False) 
     df=pd.merge(df,df2,on='Num_Acc')
-    file_name3="usagers_"+year+".csv"
+    file_name3="usagers_"+str(year)+".csv"
     df2= pd.read_csv(path+file_name3, sep=',',low_memory=False)
     if int(year)<2019:
         df2['grav'] = df2['grav'].replace({2: 4, 4: 2})
         df2['grav'] = df2['grav'].replace({'2': 4, '4': 2})
+        df2['grav'] = df2['grav'].replace({'1':1,'2':2,'3':3,'4':4})
     grav_df = df2.groupby('Num_Acc')['grav'].max().reset_index()
     df=pd.merge(df,grav_df,on='Num_Acc')
     df=df.drop_duplicates()
-    file_name4="vehicules_"+year+".csv"
+    file_name4="vehicules_"+str(year)+".csv"
     return df
+
 
 def complete_dataset():
     df=dataset_per_year("2005")
     for i in range (2006,2019):
-        df=pd.concat([df,dataset_per_year(str(i))])
+        df=pd.concat([df,dataset_per_year(i)])
         print(i)
     df=df.drop('env1',axis=1)
     for i in range (2019,2023):
-        df=pd.concat([df,dataset_per_year(str(i))])
+        df=pd.concat([df,dataset_per_year(i)])
         print(i)
     return df
 
