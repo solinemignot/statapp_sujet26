9d654c2a8b99071bb5af5cdf275912e43e040a0b
garance-lat <151042780+garance-lat@users.noreply.github.com>
2024-03-19 21:59:38 +0100

    Update dummy_2019.py

--- Partie2_ML/dummy_2019.py
+++ Partie2_ML/dummy_2019.py
@@ -31,14 +31,13 @@ df = pd.get_dummies(df, columns=['jour_de_semaine'], prefix='jour', dtype=int)
 
 #Dummy plage horaire
 '''def plage_horaire(heure):
-    heure=heure[:2]
-    if '05' <= heure < '12':
+    if 5 <= heure < 12:
         return 'matin'
-    elif '12' <= heure < '14':
+    elif 12 <= heure < 14:
         return 'midi'
-    elif '14' <= heure < '18':
+    elif 14 <= heure < 18:
         return 'après-midi'
-    elif '18' <= heure < '22':
+    elif 18 <= heure < 22:
         return 'soir'
     else:
         return 'nuit'
@@ -84,19 +83,144 @@ for start, end in vacances_2019:
 df['mois'] = df['mois'].replace({1.:'janvier',2.:'février',3.:'mars',4.:'avril',5.:'mai',6.:'juin',7.:'juillet',8.:'août',9.:'septembre',10.:'octobre',11.:'novembre',12.:'décembre'})
 df = pd.get_dummies(df, columns=['mois'], prefix='mois', dtype=int)
 
-# Dummy pour les variables spécifiées, en excluant les valeurs égales à -1
-columns_to_dummy = ['lum', 'agg', 'dep', 'int', 'atm', 'catr', 'circ', 'nbv', 'prof', 'plan', 'surf', 'situ', 'infra', 'vma']
+# Dummy pour les variables spécifiées, en excluant les valeurs égales à -1 : celles qui ont pas de sens (valeur num )
+columns_to_dummy = ['lum', 'agg', 'int', 'atm', 'catr', 'circ', 'prof', 'plan', 'surf', 'situ', 'infra']
 for column in columns_to_dummy:
     if -1 in df[column].unique():
         df[column] = df[column].replace(-1, pd.NA)
     else:
         df = pd.get_dummies(df, columns=[column], prefix=[column], dtype=int)
 
+# Dummy département
+def assign_region(dep_code):
+    regions = {
+        '1': 'Auvergne-Rhône-Alpes',
+        '2': 'Hauts-de-France',
+        '3': 'Auvergne-Rhône-Alpes',
+        '4': 'PACA',
+        '5': 'PACA',
+        '6': 'PACA',
+        '7': 'Auvergne-Rhône-Alpes',
+        '8': 'Grand Est',
+        '9': 'Occitanie',
+        '10': 'Grand Est',
+        '11': 'Occitanie',
+        '12': 'Occitanie',
+        '13': 'PACA',
+        '14': 'Normandie',
+        '15': 'Auvergne-Rhône-Alpes',
+        '16': 'Nouvelle-Aquitaine',
+        '17': 'Nouvelle-Aquitaine',
+        '18': 'Centre-Val de Loire',
+        '19': 'Nouvelle-Aquitaine',
+        '2A': 'Corse',
+        '2B': 'Corse',
+        '21': 'Bourgogne-Franche-Comté',
+        '22': 'Bretagne',
+        '23': 'Nouvelle-Aquitaine',
+        '24': 'Nouvelle-Aquitaine',
+        '25': 'Bourgogne-Franche-Comté',
+        '26': 'Auvergne-Rhône-Alpes',
+        '27': 'Normandie',
+        '28': 'Centre-Val de Loire',
+        '29': 'Bretagne',
+        '30': 'Occitanie',
+        '31': 'Occitanie',
+        '32': 'Occitanie',
+        '33': 'Nouvelle-Aquitaine',
+        '34': 'Occitanie',
+        '35': 'Bretagne',
+        '36': 'Centre-Val de Loire',
+        '37': 'Centre-Val de Loire',
+        '38': 'Auvergne-Rhône-Alpes',
+        '39': 'Bourgogne-Franche-Comté',
+        '40': 'Nouvelle-Aquitaine',
+        '41': 'Centre-Val de Loire',
+        '42': 'Auvergne-Rhône-Alpes',
+        '43': 'Auvergne-Rhône-Alpes',
+        '44': 'Pays de la Loire',
+        '45': 'Centre-Val de Loire',
+        '46': 'Occitanie',
+        '47': 'Nouvelle-Aquitaine',
+        '48': 'Occitanie',
+        '49': 'Pays de la Loire',
+        '50': 'Normandie',
+        '51': 'Grand Est',
+        '52': 'Grand Est',
+        '53': 'Pays de la Loire',
+        '54': 'Grand Est',
+        '55': 'Grand Est',
+        '56': 'Bretagne',
+        '57': 'Grand Est',
+        '58': 'Bourgogne-Franche-Comté',
+        '59': 'Hauts-de-France',
+        '60': 'Hauts-de-France',
+        '61': 'Normandie',
+        '62': 'Hauts-de-France',
+        '63': 'Auvergne-Rhône-Alpes',
+        '64': 'Nouvelle-Aquitaine',
+        '65': 'Occitanie',
+        '66': 'Occitanie',
+        '67': 'Grand Est',
+        '68': 'Grand Est',
+        '69': 'Auvergne-Rhône-Alpes',
+        '70': 'Bourgogne-Franche-Comté',
+        '71': 'Bourgogne-Franche-Comté',
+        '72': 'Pays de la Loire',
+        '73': 'Auvergne-Rhône-Alpes',
+        '74': 'Auvergne-Rhône-Alpes',
+        '75': 'Île-de-France',
+        '76': 'Normandie',
+        '77': 'Île-de-France',
+        '78': 'Île-de-France',
+        '79': 'Nouvelle-Aquitaine',
+        '80': 'Hauts-de-France',
+        '81': 'Occitanie',
+        '82': 'Occitanie',
+        '83': 'PACA',
+        '84': 'PACA',
+        '85': 'Pays de la Loire',
+        '86': 'Nouvelle-Aquitaine',
+        '87': 'Nouvelle-Aquitaine',
+        '88': 'Grand Est',
+        '89': 'Bourgogne-Franche-Comté',
+        '90': 'Bourgogne-Franche-Comté',
+        '91': 'Île-de-France',
+        '92': 'Île-de-France',
+        '93': 'Île-de-France',
+        '94': 'Île-de-France',
+        '95': 'Île-de-France',
+        '971': 'Outre Mer',
+        '972': 'Outre Mer',
+        '973': 'Outre Mer',
+        '974': 'Outre Mer',
+        '976': 'Outre Mer',
+        '977' : 'Collectivité Outre Mer', 
+        '978' : 'Collectivité Outre Mer', 
+        '986' : 'Collectivité Outre Mer', 
+        '987' : 'Collectivité Outre Mer', 
+        '988' : 'Collectivité Outre Mer',
+        '2A' : 'Corse',
+        '2B' : 'Corse',
+
+    }
+    return regions.get(dep_code[:2], 'Autre')
+
+df['region'] = df['dep'].apply(assign_region)
+#j'ai verif tout a été fill out avec les régions (pas de valeurs manquantes ds région)
+
+df = pd.get_dummies(df, columns=['region'], prefix='region', dtype=int)
+
 # Enlever les colonnes spécifiées
-columns_to_drop = ['Num_Acc', 'an', 'jour', 'hrmn', 'col', 'com', 'adr', 'gps', 'lat', 'long', 'voie', 'v1', 'v2', 'pr', 'pr1', 'vosp', 'lartpc', 'larrout', 'date']
+columns_to_drop = ['Num_Acc', 'an', 'jour', 'hrmn', 'col', 'dep','com', 'adr', 'gps', 'lat', 'long', 'voie', 'v1', 'v2', 'pr', 'pr1', 'vosp', 'lartpc', 'larrout', 'date']
 df.drop(columns=columns_to_drop, inplace=True)
 
 # Enregistrer le DataFrame dans un nouveau fichier CSV
 output_file = "/home/onyxia/work/statapp_sujet26/dataset_2019_dummy.csv"
 df.to_csv(output_file, index=False)
 
+
+
+# Afficher les premières lignes avec toutes les colonnes visibles
+with pd.option_context('display.max_columns', None):
+    print(df.head())
